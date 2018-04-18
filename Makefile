f0a0c47345ee3616597d008d394dae0179a474d4
Ricky Wong <wwcwong@ucdavis.edu>
2018-04-18 11:33:49 -0700

    first

---
+++ Makefile
@@ -0,0 +1,35 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: wawong <marvin@42.fr>                      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/04/16 16:23:54 by wawong            #+#    #+#              #
+#    Updated: 2018/04/17 18:24:22 by wawong           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+CC = gcc
+
+NAME = libft.a
+
+SRC = ft_tolower.c ft_toupper.c ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_strcmp.c ft_strncmp.c ft_atoi.c ft_strlen.c ft_strdup.c ft_strcpy.c ft_strncpy.c ft_strstr.c ft_strnstr.c ft_strcat.c ft_strncat.c ft_strlcat.c ft_strchr.c ft_strrchr.c ft_putchar.c ft_putstr.c ft_putchar_fd.c ft_putstr_fd.c ft_strclr.c ft_putendl.c ft_putnbr.c ft_putnbr_fd.c ft_putendl_fd.c ft_striter.c ft_striteri.c ft_strequ.c ft_strnequ.c ft_itoa.c ft_memset.c ft_bzero.c
+
+OBJECT = ft_tolower.o ft_toupper.o ft_isalpha.o ft_isdigit.o ft_isalnum.o ft_isascii.o ft_isprint.o ft_strcmp.o ft_strncmp.o ft_atoi.o ft_strlen.o ft_strdup.o ft_strcpy.o ft_strncpy.o ft_strstr.o ft_strnstr.o ft_strcat.o ft_strncat.o ft_strlcat.o ft_strchr.o ft_strrchr.o ft_putchar.o ft_putstr.o ft_putchar_fd.o ft_putstr_fd.o ft_strclr.o ft_putendl.o ft_putnbr.o ft_putnbr_fd.o ft_putendl_fd.o ft_striter.o ft_striteri.o ft_strequ.o ft_strnequ.o ft_itoa.o ft_memset.o ft_bzero.o
+
+CFLAGS = -c -Wall -Wextra -Werror -I libft.h
+
+all: $(NAME)
+
+$(NAME):
+	@$(CC) $(CFLAGS) $(SRC) && ar rc $(NAME) $(OBJECT)
+
+clean:
+	@rm -f $(OBJECT)
+
+fclean: clean
+	@rm -f $(NAME)
+
+re: fclean all
+	@rm -f $(OBJECT)
---
+++ libft.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wawong <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/16 17:00:26 by wawong            #+#    #+#             */
+/*   Updated: 2018/04/17 18:24:04 by wawong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+
+char		*ft_strdup(const char *s1);
+char		*ft_strcpy(char *dest, char *src);
+char		*ft_strncpy(char *dst, const char *src, size_t len);
+char		*ft_strstr(const char *haystack, const char *needle);
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char		*ft_strcat(char *s1, const char *s2);
+char		*ft_strncat(char *s1, const char *s2, size_t n);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
+char		*ft_itoa(int n);
+int			ft_isalpha(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_strcmp(const char *s1, const char *s2);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_atoi(const char *str);
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcat(char *dst, const char *src, size_t n);
+void		ft_putchar(char c);
+void		ft_putstr(char const *s);
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char const *s, int fd);
+void		ft_putendl_fd(char const *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
+void		ft_putendl(char const *s);
+void		ft_putnbr(int n);
+void		ft_strclr(char *s);
+void		ft_striter(char *s, void (*f)(char*));
+void		ft_striteri(char *s, void (*f)(unsigned int, char*));
+void		*ft_memset(void *b, int c, size_t len);
+void		ft_bzero(void *s, size_t n);
+int			ft_strequ(char const *s1, char const *s2);
+int			ft_strnequ(char const *s1, char const *s2, size_t len);
+
+#endif
